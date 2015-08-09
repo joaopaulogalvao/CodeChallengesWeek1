dd93094e6778cb5faa5a7afa932683c12d184025
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-08-09 16:58:57 -0700

    Finished codeChallenge2.

--- CodeChallenge2.playground/Contents.swift
+++ CodeChallenge2.playground/Contents.swift
@@ -2,4 +2,15 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+for index in 1...100 {
+  
+  if index%3 == 0 && index%5 == 0{
+    println("fizzbuzz")
+  } else if index%3 == 0{
+    println("fizz")
+  } else if index%5 == 0{
+    println("buzz")
+  }
+ 
+}
